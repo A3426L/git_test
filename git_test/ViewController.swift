51a8bca093df54a29933e48141b1654ddad87232
A3426L <mikan7738@gmail.com>
2024-05-03 18:40:13 +0900

    Initial Commit

---
+++ git_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git_test
+//
+//  Created by aru on 2024/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
